d27f8e2fc724c4e5b6216579830af76172f9922e
Andriy <grushevoyandrey@gmail.com>
2024-11-10 21:02:32 +0000

    py aqua

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from abc import ABC, abstractmethod
+from abc import ABC
 
 
 class IntegerRange:
@@ -21,16 +21,21 @@ class IntegerRange:
             instance: "SlideLimitationValidator",
             value: int
     ) -> None:
+        if not isinstance(value, int):
+            raise TypeError(
+                f"The value for {self.protected_name} must be an integer."
+            )
         if not (self.min_amount <= value <= self.max_amount):
-            raise ValueError(f"{self.protected_name} must be between {self.min_amount}"
-                             f" and {self.max_amount}")
+            raise ValueError(
+                f"The value for {self.protected_name}"
+                f" must be between {self.min_amount} and {self.max_amount}."
+            )
         setattr(instance, self.protected_name, value)
 
 
 class Visitor:
     def __init__(
-            self,
-            name: str,
+            self, name: str,
             age: int,
             weight: int,
             height: int
@@ -42,51 +47,34 @@ class Visitor:
 
 
 class SlideLimitationValidator(ABC):
-    def __init__(
-            self,
-            age_range: IntegerRange,
-            weight_range: IntegerRange,
-            height_range: IntegerRange
-    ) -> None:
-        self.age_range = age_range
-        self.weight_range = weight_range
-        self.height_range = height_range
-
-    @abstractmethod
-    def validate(self, visitor: "Visitor") -> bool:
-        pass
+    pass
 
 
 class ChildrenSlideLimitationValidator(SlideLimitationValidator):
-    def __init__(self) -> None:
-        super().__init__(
-            age_range=IntegerRange(4, 14),
-            weight_range=IntegerRange(20, 50),
-            height_range=IntegerRange(80, 120)
-        )
+    age = IntegerRange(4, 14)
+    weight = IntegerRange(20, 50)
+    height = IntegerRange(80, 120)
 
-    def validate(self, visitor: "Visitor"):
-        return (
-                self.age_range.min_amount <= visitor.age <= self.age_range.max_amount and
-                self.weight_range.min_amount <= visitor.weight <= self.weight_range.max_amount and
-                self.height_range.min_amount <= visitor.height <= self.height_range.max_amount
-        )
+    def __init__(
+            self,
+            age: int,
+            weight: int,
+            height: int
+    ) -> None:
+        self.age = age
+        self.weight = weight
+        self.height = height
 
 
 class AdultSlideLimitationValidator(SlideLimitationValidator):
-    def __init__(self) -> None:
-        super().__init__(
-            age_range=IntegerRange(14, 60),
-            weight_range=IntegerRange(50, 120),
-            height_range=IntegerRange(120, 220)
-        )
+    age = IntegerRange(14, 60)
+    weight = IntegerRange(50, 120)
+    height = IntegerRange(120, 220)
 
-    def validate(self, visitor: "Visitor") -> None:
-        return (
-                self.age_range.min_amount <= visitor.age <= self.age_range.max_amount and
-                self.weight_range.min_amount <= visitor.weight <= self.weight_range.max_amount and
-                self.height_range.min_amount <= visitor.height <= self.height_range.max_amount
-        )
+    def __init__(self, age: int, weight: int, height: int) -> None:
+        self.age = age
+        self.weight = weight
+        self.height = height
 
 
 class Slide:
@@ -96,7 +84,12 @@ class Slide:
             limitation_class: SlideLimitationValidator
     ) -> None:
         self.name = name
-        self.limitation_class = limitation_class()
+        self.limitation_class = limitation_class
 
     def can_access(self, visitor: "Visitor") -> bool:
-        return self.limitation_class.validate(visitor)
+        try:
+            self.limitation_class(visitor.age, visitor.weight, visitor.height)
+            return True
+        except (TypeError, ValueError) as e:
+            print(f"Access denied: {e}")
+            return False
